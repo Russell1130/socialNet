04762155ce4da4a86dc72aa55debc12116cf9ec0
Russell1130 <devlord0625@gmail.com>
2023-03-07 07:41:28 -0500

    first commit

---
+++ truffle-config.js
@@ -0,0 +1,32 @@
+/*
+require('babel-register');
+require('babel-polyfill');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+require('dotenv').config();
+*/
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+/*    goerli: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://goerli.infura.io/v3/${process.env.PROJECT_ID}`),
+      network_id: 5,       // Goerli's id
+      confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },*/
+  },
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
